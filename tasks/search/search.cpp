4aa848a14b7e73f186abe8c938be49c14def99e1
Dmitry Levanov <levanovd@gmail.com>
2023-02-01 22:23:46 +0100

    Add homework two

---
+++ tasks/search/search.cpp
@@ -0,0 +1,5 @@
+#include "search.h"
+
+std::vector<std::string_view> Search(std::string_view text, std::string_view query, size_t results_count) {
+    return {};
+}
---
+++ tasks/word2vec/word2vec.cpp
@@ -0,0 +1,8 @@
+#include "word2vec.h"
+
+#include <vector>
+
+std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
+                                          const std::vector<std::vector<int>>& vectors) {
+    return {};
+}
