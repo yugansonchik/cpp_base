5b40c1e6931a19771f08f74d2c3dc8352fee479b
Dmitry Levanov <levanovd@gmail.com>
2023-02-04 16:36:16 +0000

    Update clang-tidy and clang-format to 14

--- run-clang-format.py
+++ run-clang-format.py
@@ -208,7 +208,7 @@ def main():
         '--clang-format-executable',
         metavar='EXECUTABLE',
         help='path to the clang-format executable',
-        default='clang-format-11')
+        default='clang-format-14')
     parser.add_argument(
         '--extensions',
         help='comma separated list of file extensions (default: {})'.format(
