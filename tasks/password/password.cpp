8e1721f258a91e5bdb45368c72651687d609c3da
Natalya <ugansonnatala86@gmail.com>
2023-02-05 22:31:40 +0300

    Solved task password

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,5 +1,44 @@
 #include "password.h"
+#include <cctype>
+#include <iostream>
+#include <string>
+
+const int32_t MIN_LENGTH = 8;
+const int32_t MAX_LENGTH = 14;
+
+const int32_t MIN_ASCII = 33;
+const int32_t MAX_ASCII = 126;
 
 bool ValidatePassword(const std::string& password) {
-    return false;
+    int len_of_password = password.length();
+    if (len_of_password < MIN_LENGTH || password.size() > MAX_LENGTH) {
+        return false;
+    }
+    bool digit = false;
+    bool upper = false;
+    bool lower = false;
+    bool other = false;
+
+
+    for (char p : password) {
+        if (p < MIN_ASCII || p > MAX_ASCII) {
+            return false;
+        } else if (std::isdigit(p) && !digit) {
+            digit = true;
+        } else if (std::isupper(p) && !upper) {
+            upper = true;
+        } else if (std::islower(p) && !lower) {
+            lower = true;
+        } else {
+            other = 1;
+        }
+    }
+    return digit + upper + lower + other >= 3;
+}
+
+int main() {
+    std::string password = "";
+    std::getline(std::cin, password);
+    std::cout << ValidatePassword(password) << "\n";
+    return 0;
 }
