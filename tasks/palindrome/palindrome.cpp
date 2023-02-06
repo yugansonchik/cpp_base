65da46efad0923c03e986adb759386251868513d
Natalya <nyuganson@mail.ru>
2023-02-06 17:49:57 +0300

    attempt

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -7,5 +7,5 @@ bool IsPalindrome(const std::string& str) {
     std::string str_copy = str;
     std::getline(std::cin, str_copy);
     str_copy.erase(std::remove(str_copy.begin(), str_copy.end(), ' '), str_copy.end());
-    return (!std::equal(str_copy.begin(), str_copy.begin() + static_cast<int>(str_copy.size()) / 2, str_copy.rbegin()));
+    return std::equal(str_copy.begin(), str_copy.begin() + static_cast<int>(str_copy.size()) / 2, str_copy.rbegin());
 }
