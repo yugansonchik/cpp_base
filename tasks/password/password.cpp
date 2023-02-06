53e4436bfc46aec247fe5a975e0b341945e5ac08
Natalya <nyuganson@mail.ru>
2023-02-06 16:45:07 +0300

    yahoo!

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -3,29 +3,31 @@
 #include <iostream>
 #include <string>
 
-const int32_t MIN_LENGTH = 8;
-const int32_t MAX_LENGTH = 14;
-const int32_t MIN_ASCII = 33;
-const int32_t MAX_ASCII = 126;
 bool ValidatePassword(const std::string& password) {
-    if (password.size() < MIN_LENGTH || password.size() > MAX_LENGTH) {
+
+    const int32_t min_length = 8;
+    const int32_t max_length = 14;
+    const char min_ascii = 33;
+    const char max_ascii = 126;
+
+    if (password.size() < min_length || password.size() > max_length) {
         return false;
     }
-    bool digit = false;
-    bool upper = false;
-    bool lower = false;
-    bool other = false;
+    int digit = 0;
+    int upper = 0;
+    int lower = 0;
+    int other = 0;
     for (char p : password) {
-        if (p < MIN_ASCII || p > MAX_ASCII) {
+        if (p < min_ascii || p > max_ascii) {
             return false;
         } else if (std::isdigit(p) && !digit) {
-            digit = true;
+            digit = 1;
         } else if (std::isupper(p) && !upper) {
-            upper = true;
+            upper = 1;
         } else if (std::islower(p) && !lower) {
-            lower = true;
+            lower = 1;
         } else {
-            other = true;
+            other = 1;
         }
     }
     return digit + upper + lower + other >= 3;
