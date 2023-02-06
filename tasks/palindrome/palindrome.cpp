ef31b8dd4702bf68e9931ddde8d5ded0eee4a567
Natalya <nyuganson@mail.ru>
2023-02-06 17:22:50 +0300

    ооо

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,11 @@
 #include "palindrome.h"
+#include <algorithm>
+#include <iostream>
+#include <string>
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    std::string str_copy = str;
+    std::getline(std::cin, str_copy);
+    str_copy.erase(std::remove(str_copy.begin(), str_copy.end(), ' '), str_copy.end());
+    return (!(std::equal(str_copy.begin(), str_copy.begin() + str_copy.size() / 2, str_copy.rbegin())));
 }
