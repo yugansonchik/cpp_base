29d83e4d668e9817bf4df8bc040c870a24f087d3
Natalya <nyuganson@mail.ru>
2023-02-06 19:29:16 +0300

    resolved

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -4,7 +4,7 @@ bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
     int32_t cnt_of_rows = static_cast<int>(table.size());
     for (size_t i = 0; i < cnt_of_rows; ++i) {
         for (size_t j = 0; j < cnt_of_rows; ++j) {
-            for(size_t k = 0; k < cnt_of_rows; ++k) {
+            for (size_t k = 0; k < cnt_of_rows; ++k) {
                 if (table[table[i][j]][k] != table[i][table[j][k]]) {
                     return false;
                 }
@@ -12,3 +12,4 @@ bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
         }
     }
     return true;
+}
