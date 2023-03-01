e366be16dce19f6d83d9ae1870624791cb049320
Natalya <ugansonnatala86@gmail.com>
2023-03-01 13:13:39 +0300

    ioy

--- tasks/scorer2/scorer.h
+++ tasks/scorer2/scorer.h
@@ -4,11 +4,17 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using StudentName = std::string;
 using TaskName = std::string;
 
 using ScoreTable = std::map<StudentName, std::set<TaskName>>;
+
+struct InfoAboutTask {
+    bool check_success = false;
+    bool merge_closed = true;
+};
 
 class Scorer {
 public:
@@ -17,6 +23,11 @@ public:
     void OnMergeRequestOpen(const StudentName& student_name, const TaskName& task_name);
     void OnMergeRequestClosed(const StudentName& student_name, const TaskName& task_name);
     void Reset();
+    void AddToScoreTable(const StudentName& student_name, const TaskName& task_name);
 
     ScoreTable GetScoreTable() const;
+
+private:
+    ScoreTable score_table_;
+    std::unordered_map<std::string, std::unordered_map<std::string, InfoAboutTask>> student_task_;
 };
