22f724d110d0ace2ef216d7b8ae9e3f631c5d7ab
Natalya <ugansonnatala86@gmail.com>
2023-03-10 03:49:53 +0300

    lolo

--- tasks/mines/minesweeper.h
+++ tasks/mines/minesweeper.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <deque>
 #include <string>
 #include <vector>
 
@@ -32,4 +33,25 @@ public:
     time_t GetGameTime() const;
 
     RenderedField RenderField() const;
+    ~Minesweeper();
+
+private:
+    GameStatus game_status_ = GameStatus::NOT_STARTED;
+    time_t start_time_;
+    time_t end_time_;
+    size_t mines_count_;
+    size_t opened_count_;
+    struct CellData {
+        bool is_mined = false;
+        bool is_flagged = false;
+        bool is_opened = false;
+    };
+    size_t width_ = 0;
+    size_t height_ = 0;
+    CellData** field_ = nullptr;
+    int CountMines(size_t x, size_t y) const;
+    void FreeMemory();
+    void AllocateMemory();
+    void SetMinesRandomly();
+    std::deque<Cell> cells_to_open_;
 };
