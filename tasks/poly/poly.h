45440b8b658c7466b0ed6f8e4449a564889dcf25
alpashka1 <127903983+alpashka1@users.noreply.github.com>
2023-03-26 23:53:14 +0300

    dkd

--- tasks/poly/poly.h
+++ tasks/poly/poly.h
@@ -1,4 +1,35 @@
 #pragma once
 
+#include <cstdint>
+#include <map>
+#include <sstream>
+
 class Poly {
+public:
+    Poly();
+    Poly(const std::initializer_list<int64_t>& initializer_list);
+    Poly(const std::initializer_list<std::pair<int64_t, int64_t>>& initializer_pair_list);
+
+    int64_t operator()(int64_t x) const;
+
+    bool operator==(const Poly& other) const;
+    bool operator!=(const Poly& other) const;
+
+    Poly operator-(const Poly& other) const;
+    const Poly& operator-=(const Poly& other);
+
+    Poly operator+(const Poly& other) const;
+    const Poly& operator+=(const Poly& other);
+
+    Poly operator-() const;
+
+    Poly operator*(const Poly& other) const;
+    const Poly& operator*=(const Poly& other);
+
+    friend std::ostream& operator<<(std::ostream& os, const Poly& poly);
+
+private:
+    std::map<uint64_t, int64_t> poly_;
+
+    void Normalize();
 };
